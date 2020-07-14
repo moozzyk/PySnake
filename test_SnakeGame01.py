416780a3019e276fc4d6dd839300233370cae6df
moozzyk <moozzyk@gmail.com>
2020-07-13 18:41:07 -0700

    Updating the test to have non-empty list

--- test_SnakeGame01.py
+++ test_SnakeGame01.py
@@ -20,11 +20,12 @@ class TestSnakeGame01(unittest.TestCase):
         Look at the __init__ method int SnakeGame.py. file. The method has 3 parameters
         (food, snake, direction) that have assigned default values. The default values
         will be used if no values were provided for these parameters - like in the test
-        above. Initialize corresponding class variables using these parameters.
+        above. Initialize corresponding class variables using these parameters. Don't worry
+        about values for now.
         """
-        game = SnakeGame(10, 15, food=[], snake=[], direction=DOWN)
-        self.assertEqual([], game.food)
-        self.assertEqual([], game.snake)
+        game = SnakeGame(10, 15, food=[(5, 2)], snake=[(4, 6)], direction=DOWN)
+        self.assertEqual([(5, 2)], game.food)
+        self.assertEqual([(4, 6)], game.snake)
         self.assertEqual(DOWN, game.direction)
 
 
